9616fd07ba7635a71963ec9da943db64f9c8c35d
Qulle <qulle973@gmail.com>
2022-05-13 21:23:56 +0200

    Added core module with components and implemented simple header and menu design

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        MenuComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        HeaderComponent,
+        MenuComponent
+    ]
+})
+export class CoreModule { }
